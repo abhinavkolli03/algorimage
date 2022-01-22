08303f4acc00ecef77ce154978b9a20dc14ab6b8
abhinavkolli03 <abhinavkolli03@gmail.com>
2022-01-21 23:37:49 -0600

    push for deployment

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -3,7 +3,6 @@ import React from 'react'
 const HomePage = () => {
     return (
         <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '90vh'}}>
-            <h1>Home</h1>
         </div>
     )
 }
